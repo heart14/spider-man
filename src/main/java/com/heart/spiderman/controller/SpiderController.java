e65d4cd4ef13084e25a019fecd49a5ec705da844
liHeart <lwf14@qq.com>
2019-05-21 14:24:51 +0800

    spider man

--- src/main/java/com/heart/spiderman/controller/SpiderController.java
+++ src/main/java/com/heart/spiderman/controller/SpiderController.java
@@ -8,15 +8,12 @@ import com.heart.spiderman.utils.URLParseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @ClassName:SpiderController
@@ -37,29 +34,54 @@ public class SpiderController {
 
     /**
      * 接收前端传入的问题ID 启动spider获取该问题下所有答案内容
+     *
      * @param questionId
      */
-    @RequestMapping(value = "/spider/{questionId}", method = RequestMethod.POST)
-    public void spiderManRun(@PathVariable("questionId") String questionId) {
-        logger.info("questionId = {}", questionId);
+    @RequestMapping(value = "/spider/{questionId}", method = RequestMethod.GET)
+    @ResponseBody
+    public List<String> spiderManRun(@PathVariable("questionId") String questionId) {
+        logger.info("questionId = {}，开始获取图片......", questionId);
         try {
-            List<Answer> answerList = HttpUtils.spiderMan(new Spider().buildSpider(questionId));
-
-            for (Answer answer : answerList) {
+            List<Answer> answerList = HttpUtils.spiderMan(new Spider().buildSpider(questionId));//获取所有回答
+            ExecutorService executorService = Executors.newFixedThreadPool(8);//创建线程池
+            int totalPhoto = 0;//接收总图片数
+            int totalAnonymous = 0;//接收匿名用户数
+            int totalEmpty = 0;//接收无图片回答数
+            List<String> urls = new ArrayList<>();//接收所有图片url
+            List<Map<String, String>> answerUrlMapList = new ArrayList<>();
+            String questionTitle = answerList.get(0).getQuestion().getTitle();//保存问题标题
+            for (Answer answer : answerList) {//遍历所有回答，获取所有图片url集合
+                String authorName = answer.getAuthor().getName();
+                if ("匿名用户".equals(authorName)) {
+                    totalAnonymous += 1;
+                }
                 List<String> urlList = URLParseUtils.doRegexMatcher(answer.getContent());
-                if (urlList != null && urlList.size() > 0) {
-                    List<String> urls = new ArrayList<>(new HashSet<>(urlList));
-                    FileDownloadUtils fileDownloadUtils = new FileDownloadUtils(answer.getQuestion().getTitle(), answer.getAuthor().getName(), urls);
-                    Thread thread = new Thread(fileDownloadUtils);
-                    thread.start();
+                List<String> urlListTrim = new ArrayList<>(new HashSet<>(urlList));
+                if (urlListTrim != null && urlListTrim.size() > 0) {
+                    totalPhoto += urlListTrim.size();
+                    urls.addAll(urlListTrim);
+                    for (String url : urlListTrim) {//遍历每个问题下的图片url集合，获取authorName:url键值对保存到map
+                        Map<String, String> answerUrlMap = new HashMap<>();//接收authorName:url键值对
+                        answerUrlMap.put("authorName", authorName);
+                        answerUrlMap.put("url", url);
+                        answerUrlMapList.add(answerUrlMap);
+                    }
+                } else {
+                    totalEmpty += 1;
                 }
             }
-
+            logger.info("共{} 条回答，{} 纯文字回答，{} 匿名用户，{} 图片，开始下载......", answerList.size(), totalEmpty, totalAnonymous, totalPhoto);
+            for (Map<String, String> map : answerUrlMapList) {
+                String authorName = map.get("authorName");
+                String url = map.get("url");
+                    FileDownloadUtils fileDownloadUtils = new FileDownloadUtils(questionTitle, authorName, url);
+                    executorService.execute(fileDownloadUtils);
+            }
+            return urls;
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
-
-
+        return null;
     }
 
 }
--- src/main/java/com/heart/spiderman/utils/FileDownloadUtils.java
+++ src/main/java/com/heart/spiderman/utils/FileDownloadUtils.java
@@ -8,7 +8,6 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 /**
  * @ClassName:FileDownloadUtils
@@ -22,63 +21,67 @@ public class FileDownloadUtils implements Runnable {
 
     private String questionTitle;
     private String authorName;
-    private List<String> urls;
+    private String url;
 
     public FileDownloadUtils() {
     }
 
-    public FileDownloadUtils(String questionTitle, String authorName, List<String> urls) {
+    public FileDownloadUtils(String questionTitle, String authorName, String url) {
         this.questionTitle = questionTitle;
         this.authorName = authorName;
-        this.urls = urls;
+        this.url = url;
     }
 
     @Override
     public void run() {
-        String filePath = SpiderConfig.FILE_TARGET_PATH + questionTitle;
+
+        String filePath = SpiderConfig.FILE_TARGET_PATH + this.questionTitle;//构造图片保存文件夹，以问题标题为名
         File file = new File(filePath);
-        if (!file.exists()) {
+        if (!file.exists()) {//文件夹不存在则创建文件夹
             file.mkdirs();
         }
 
-        for (String urlStr : urls) {
+        String photoPath = filePath + "/" + this.authorName;//构造图片保存文件夹，以作者昵称为名
+        File photoDir = new File(photoPath);
+        if (!photoDir.exists()) {//文件夹不存在则创建文件夹
+            photoDir.mkdirs();
+        }
+        File photo = new File(photoPath + "/" + URLParseUtils.doFetchUID(this.url));//构造图片文件
+        if (photo.exists()) {//如果图片文件已存在，则忽略
+            logger.info("[{}][{}][{}] 图片已存在", Thread.currentThread().getName(), this.authorName, URLParseUtils.doFetchUID(this.url));
+            return;
+        }
 
-            File photo = new File(filePath + "/" + authorName + "_" + URLParseUtils.doFetchUID(urlStr));
-            if (photo.exists()) {
-                continue;
+        BufferedOutputStream bufferedOutputStream = null;
+        try {
+            URL url = new URL(this.url);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(25 * 1000);
+            connection.setReadTimeout(25 * 1000);
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+                logger.info("[{}][{}][{}] 下载失败，请求错误", Thread.currentThread().getName(), this.authorName, URLParseUtils.doFetchUID(this.url));
+                return;
             }
-
-            BufferedOutputStream bufferedOutputStream = null;
-            try {
-                URL url = new URL(urlStr);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setConnectTimeout(5 * 1000);
-                connection.setReadTimeout(5 * 1000);
-                int responseCode = connection.getResponseCode();
-                if (responseCode != 200) {
-                    logger.info("[" + authorName + "]  \t{}", "下载失败，请求错误" + URLParseUtils.doFetchUID(urlStr));
-                    continue;
-                }
-                logger.info("[" + authorName + "]  \t{}", "正在下载" + URLParseUtils.doFetchUID(urlStr));
-                InputStream inputStream = connection.getInputStream();
-                bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(photo));
-                int count;
-                byte[] bytes = new byte[2048];
-                while ((count = inputStream.read(bytes)) != -1) {
-                    bufferedOutputStream.write(bytes, 0, count);
-                }
-                bufferedOutputStream.flush();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (bufferedOutputStream != null) {
-                    try {
-                        bufferedOutputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            logger.info("[{}][{}][{}] 下载完成", Thread.currentThread().getName(), this.authorName, URLParseUtils.doFetchUID(this.url));
+            InputStream inputStream = connection.getInputStream();
+            bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(photo));
+            int count;
+            byte[] bytes = new byte[2048];
+            while ((count = inputStream.read(bytes)) != -1) {
+                bufferedOutputStream.write(bytes, 0, count);
+            }
+            bufferedOutputStream.flush();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedOutputStream != null) {
+                try {
+                    bufferedOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
