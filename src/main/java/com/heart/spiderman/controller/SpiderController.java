c82cdcb011dcf3a91e52b3a423b199cd7c07c515
heart14 <lwf14@qq.com>
2019-09-03 18:33:20 +0800

    spider man

--- src/main/java/com/heart/spiderman/controller/SpiderController.java
+++ src/main/java/com/heart/spiderman/controller/SpiderController.java
@@ -33,70 +33,91 @@ public class SpiderController {
     @Autowired
     private ThreadPoolExecutor threadPoolExecutor;
 
+    @RequestMapping(value = "/spider/{questionId}/{answerNum}", method = RequestMethod.GET)
+    @ResponseBody
+    public String spiderManRun(@PathVariable("questionId") String questionId, @PathVariable("answerNum") int answerNum) {
+        logger.info("questionId = {}, 开始获取图片......", questionId);
+
+        Spider spider = new Spider().buildSpider(questionId);
+        int limit = 20;
+        int offset = 0;
+        int total = answerNum;
+        if (total - (limit + offset) != 0) {
+            if (total - (limit + offset) > 20) {
+                spider.setOffset(limit + offset);
+            } else {
+                spider.setLimit(total - (limit + offset));
+                spider.setOffset(limit + offset);
+            }
+        }
+
+        return null;
+    }
+
     /**
      * SpiderManRun
      *
      * @param questionId
      */
-    @RequestMapping(value = "/spider/{questionId}", method = RequestMethod.GET)
-    @ResponseBody
-    public String spiderManRun(@PathVariable("questionId") String questionId) {
-        logger.info("questionId = {}，开始获取图片......", questionId);
-        JSONObject jsonObject = new JSONObject(1);
-        try {
-            //获取所有回答
-            List<Answer> answerList = HttpUtils.spiderMan(new Spider().buildSpider(questionId));
-            //接收总图片数
-            int totalPhoto = 0;
-            //接收匿名用户数
-            int totalAnonymous = 0;
-            //接收无图片回答数
-            int totalEmpty = 0;
-            //接收所有图片url
-            List<String> urls = new ArrayList<>();
-            List<Map<String, String>> answerUrlMapList = new ArrayList<>();
-
-
-            //保存问题标题
-            String questionTitle = answerList.get(0).getQuestion().getTitle();
-            //遍历所有回答，获取所有图片url集合
-            for (Answer answer : answerList) {
-                String authorName = answer.getAuthor().getName();
-                if ("匿名用户".equals(authorName)) {
-                    totalAnonymous += 1;
-                }
-                List<String> urlList = URLParseUtils.doRegexMatcher(answer.getContent());
-                List<String> urlListTrim = new ArrayList<>(new HashSet<>(urlList));
-                if (urlListTrim != null && urlListTrim.size() > 0) {
-                    totalPhoto += urlListTrim.size();
-                    urls.addAll(urlListTrim);
-                    //遍历每个问题下的图片url集合，获取authorName:url键值对保存到map
-                    for (String url : urlListTrim) {
-                        //接收authorName:url键值对
-                        Map<String, String> answerUrlMap = new HashMap<>();
-                        answerUrlMap.put("authorName", authorName);
-                        answerUrlMap.put("url", url);
-
-                        answerUrlMapList.add(answerUrlMap);
-                    }
-                } else {
-                    totalEmpty += 1;
-                }
-            }
-            logger.info("共{} 条回答，{} 纯文字回答，{} 匿名用户，{} 图片，开始下载......", answerList.size(), totalEmpty, totalAnonymous, totalPhoto);
-            for (Map<String, String> map : answerUrlMapList) {
-                String authorName = map.get("authorName");
-                String url = map.get("url");
-                FileDownloadUtils fileDownloadUtils = new FileDownloadUtils(questionTitle, authorName, url);
-                threadPoolExecutor.execute(fileDownloadUtils);
-            }
-            jsonObject.put("src", urls);
-            return jsonObject.toJSONString();
-        } catch (Exception e) {
-            logger.error("zhihu图片解析失败 :{}", e.getMessage());
-        }
-        jsonObject.put("src", "[]");
-        return jsonObject.toJSONString();
-    }
+//    @RequestMapping(value = "/spider/{questionId}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String spiderManRun(@PathVariable("questionId") String questionId) {
+//        logger.info("questionId = {}，开始获取图片......", questionId);
+//        JSONObject jsonObject = new JSONObject(1);
+//        try {
+//            //获取所有回答
+//            List<Answer> answerList = HttpUtils.spiderMan(new Spider().buildSpider(questionId));
+//            //接收总图片数
+//            int totalPhoto = 0;
+//            //接收匿名用户数
+//            int totalAnonymous = 0;
+//            //接收无图片回答数
+//            int totalEmpty = 0;
+//            //接收所有图片url
+//            List<String> urls = new ArrayList<>();
+//            List<Map<String, String>> answerUrlMapList = new ArrayList<>();
+//
+//
+//            //保存问题标题
+//            String questionTitle = answerList.get(0).getQuestion().getTitle();
+//            //遍历所有回答，获取所有图片url集合
+//            for (Answer answer : answerList) {
+//                String authorName = answer.getAuthor().getName();
+//                if ("匿名用户".equals(authorName)) {
+//                    totalAnonymous += 1;
+//                }
+//                List<String> urlList = URLParseUtils.doRegexMatcher(answer.getContent());
+//                List<String> urlListTrim = new ArrayList<>(new HashSet<>(urlList));
+//                if (urlListTrim != null && urlListTrim.size() > 0) {
+//                    totalPhoto += urlListTrim.size();
+//                    urls.addAll(urlListTrim);
+//                    //遍历每个问题下的图片url集合，获取authorName:url键值对保存到map
+//                    for (String url : urlListTrim) {
+//                        //接收authorName:url键值对
+//                        Map<String, String> answerUrlMap = new HashMap<>();
+//                        answerUrlMap.put("authorName", authorName);
+//                        answerUrlMap.put("url", url);
+//
+//                        answerUrlMapList.add(answerUrlMap);
+//                    }
+//                } else {
+//                    totalEmpty += 1;
+//                }
+//            }
+//            logger.info("共{} 条回答，{} 纯文字回答，{} 匿名用户，{} 图片，开始下载......", answerList.size(), totalEmpty, totalAnonymous, totalPhoto);
+//            for (Map<String, String> map : answerUrlMapList) {
+//                String authorName = map.get("authorName");
+//                String url = map.get("url");
+//                FileDownloadUtils fileDownloadUtils = new FileDownloadUtils(questionTitle, authorName, url);
+//                threadPoolExecutor.execute(fileDownloadUtils);
+//            }
+//            jsonObject.put("src", urls);
+//            return jsonObject.toJSONString();
+//        } catch (Exception e) {
+//            logger.error("zhihu图片解析失败 :{}", e.getMessage());
+//        }
+//        jsonObject.put("src", "[]");
+//        return jsonObject.toJSONString();
+//    }
 
 }
--- src/main/java/com/heart/spiderman/utils/HttpUtils.java
+++ src/main/java/com/heart/spiderman/utils/HttpUtils.java
@@ -7,13 +7,17 @@ import com.heart.spiderman.model.Answer;
 import com.heart.spiderman.model.Spider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @ClassName:HttpUtils
@@ -21,66 +25,78 @@ import java.util.List;
  * @Author: Heart
  * @Date: 2019/5/20 11:05
  */
-public class HttpUtils {
+public class HttpUtils implements Runnable {
 
     private static Logger logger = LoggerFactory.getLogger(HttpUtils.class);
 
-    /**
-     * http请求访问知乎API 获取问题ID下的所有答案
-     * @param spider
-     * @return
-     * @throws Exception
-     */
-    public static List<Answer> spiderMan(Spider spider) throws Exception {
+    private Spider spider;
 
-        logger.info("Search " + spider.getOffset() + "~" + (spider.getOffset() + spider.getLimit()) + " answers, please wait...");
+    @Autowired
+    private ThreadPoolExecutor threadPoolExecutor;
 
-        ArrayList<Answer> answerList = new ArrayList<>();
-        String result = "";
-        StringBuffer responseResult = new StringBuffer();
-        BufferedReader bufferedReader = null;
-        String url = spider.getUrl() + "&limit=" + spider.getLimit() + "&offset=" + spider.getOffset();
-        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestProperty("Referer", spider.getReferer());
-        connection.setRequestProperty("origin", spider.getOrigin());
-        connection.setRequestProperty("x-udid", spider.getX_udid());
-        connection.setRequestProperty("Cookie", spider.getCookie());
-        connection.setRequestProperty("accept", spider.getAccept());
-        connection.setRequestProperty("connection", spider.getConnection());
-        connection.setRequestProperty("Host", spider.getHost());
-        connection.setRequestProperty("user-agent", spider.getUser_agent());
-        connection.connect();
-        bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            responseResult.append(line);
-        }
-        result = responseResult.toString();
-        JSONObject resultObject = JSONObject.parseObject(result);
-        String resultData = resultObject.get("data").toString();
-        JSONArray dataArray = JSONArray.parseArray(resultData);
-        for (int i = 0; i < dataArray.size(); i++) {
-            Answer answer = JSON.parseObject(dataArray.get(i).toString(), Answer.class);
-            Answer.Question question = JSON.parseObject(JSONObject.parseObject(dataArray.get(i).toString()).get("question").toString(), Answer.Question.class);
-            answer.setQuestion(question);
-            Answer.Author author = JSON.parseObject(JSONObject.parseObject(dataArray.get(i).toString()).get("author").toString(), Answer.Author.class);
-            answer.setAuthor(author);
-            answerList.add(answer);
-        }
-        String paging = resultObject.get("paging").toString();
-        JSONObject page = JSONObject.parseObject(paging);
-        int total = spider.getTotal() == (int) page.get("totals") ? spider.getTotal() : (int) page.get("totals");
-        int limit = spider.getLimit();
-        int offset = spider.getOffset();
-        if (total - (limit + offset) != 0) {
-            if (total - (limit + offset) > 20) {
-                spider.setOffset(limit + offset);
-            } else {
-                spider.setLimit(total - (limit + offset));
-                spider.setOffset(limit + offset);
+    public HttpUtils() {
+    }
+
+    public HttpUtils(Spider spider) {
+        this.spider = spider;
+    }
+
+    @Override
+    public void run() {
+        try {
+            logger.info("Search " + this.spider.getOffset() + "~" + (this.spider.getOffset() + this.spider.getLimit()) + " answers, please wait...");
+
+            ArrayList<Answer> answerList = new ArrayList<>();
+            String result = "";
+            StringBuffer responseResult = new StringBuffer();
+            BufferedReader bufferedReader = null;
+            String url = this.spider.getUrl() + "&limit=" + this.spider.getLimit() + "&offset=" + this.spider.getOffset();
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestProperty("Referer", this.spider.getReferer());
+            connection.setRequestProperty("origin", this.spider.getOrigin());
+            connection.setRequestProperty("x-udid", this.spider.getX_udid());
+            connection.setRequestProperty("Cookie", this.spider.getCookie());
+            connection.setRequestProperty("accept", this.spider.getAccept());
+            connection.setRequestProperty("connection", this.spider.getConnection());
+            connection.setRequestProperty("Host", this.spider.getHost());
+            connection.setRequestProperty("user-agent", this.spider.getUser_agent());
+            connection.connect();
+            bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                responseResult.append(line);
             }
-            answerList.addAll(spiderMan(spider));
+            result = responseResult.toString();
+            JSONObject resultObject = JSONObject.parseObject(result);
+            String resultData = resultObject.get("data").toString();
+            JSONArray dataArray = JSONArray.parseArray(resultData);
+            for (int i = 0; i < dataArray.size(); i++) {
+                Answer answer = JSON.parseObject(dataArray.get(i).toString(), Answer.class);
+                Answer.Question question = JSON.parseObject(JSONObject.parseObject(dataArray.get(i).toString()).get("question").toString(), Answer.Question.class);
+                answer.setQuestion(question);
+                Answer.Author author = JSON.parseObject(JSONObject.parseObject(dataArray.get(i).toString()).get("author").toString(), Answer.Author.class);
+                answer.setAuthor(author);
+                answerList.add(answer);
+            }
+            for (Answer answer : answerList) {
+                //保存问题标题
+                String questionTitle = answer.getQuestion().getTitle();
+                //保存作者昵称
+                String authorName = answer.getAuthor().getName();
+                //获取答案中图片集合
+                List<String> urlList = URLParseUtils.doRegexMatcher(answer.getContent());
+                //去重
+                List<String> urlListTrim = new ArrayList<>(new HashSet<>(urlList));
+                //遍历图片url集合，进行下载
+                if (urlListTrim.size() > 0) {
+                    for (String s : urlListTrim) {
+                        FileDownloadUtils fileDownloadUtils = new FileDownloadUtils(questionTitle, authorName, s);
+                        threadPoolExecutor.execute(fileDownloadUtils);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.error("获取答案 {}~{} 出错 :{}", this.spider.getOffset(), this.spider.getOffset() + this.spider.getLimit(), e.getMessage());
         }
-        return answerList;
     }
 }
